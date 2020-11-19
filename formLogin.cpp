945827589d0adc80e2562d79b64fd6a478a9eab5
minhthuc2502 <minhthuc2502@gmail.com>
2020-11-20 00:17:23 +0100

    handle when image not be added

--- formLogin.cpp
+++ formLogin.cpp
@@ -3,6 +3,16 @@
 formLogin::formLogin() {
     setupUi(this);
 
+    QString imagePath = QApplication::applicationDirPath() + "/../AlooClient/img/avatar-anonyme.jpg";
+    QImage image;
+    image.load(imagePath);
+    image = image.scaledToHeight(90).scaledToWidth(84);
+    avatar = QPixmap::fromImage(image);
+    QGraphicsScene *graphicScene = new QGraphicsScene();
+    graphicScene->addPixmap(avatar);
+    graphicScene->setSceneRect(avatar.rect());
+
+    avataView->setScene(graphicScene);
     connect(closeButton, SIGNAL(clicked()), this, SLOT(HandleCloseButton()));
     connect(confirmButton, SIGNAL(clicked()), this, SLOT(saveInfo()));
     connect(addImageButton, SIGNAL(clicked(bool)), this, SLOT(addAvatar()));
@@ -13,6 +23,7 @@ void formLogin::saveInfo() {
     age = ageSpinBox->value();
     if (name.isEmpty()) {
         QMessageBox::information(this, "information", tr("You must fill all informations in this form"));
+        return;
     }
 }
 
